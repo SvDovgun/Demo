2503f4302f5fe6fb8e8897e79936072020745f70
SvDovgun <sv.dovgun@gmail.com>
2023-07-12 18:48:29 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+user_name = 'Svitlana'
+print(f"hi, {user_name}!")
+print(user_name[-2])
+
+a = 3
+b = 7
+print(a+b)
+
+my_list = ["a", "b", "c"]
+first_letter = my_list[0]
+middle_one = my_list[1]
+last_letter = my_list[2]
+print(middle_one)
